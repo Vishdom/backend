f538f13dbb99cc7049d13f8ea3c8d47570e5a0bb
vishdom <Vishpatilwork@gmail.com>
2025-09-04 19:21:31 +0530

    Learning practice assignments- conditionals

--- sql/reset.sql
+++ sql/reset.sql
@@ -1,7 +1,4 @@
 -- reset.sql
--- Drop tables if they exist
 DROP TABLE IF EXISTS posts;
 DROP TABLE IF EXISTS users;
--- Recreate schema
-.read schema.sql -- Seed data
-.read seed.sql
+.read sql / schema.sql.read sql / seed.sql
